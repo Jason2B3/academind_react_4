06b3e948700a45151f238b47d45e3a81d49a3e5e
jason <2B3>
2021-07-30 17:21:39 -0400

    commit 105: refs

--- src/components/DataForm.js
+++ src/components/DataForm.js
@@ -1,40 +1,42 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../Card";
 import styles from "./DataForm.module.scss";
 
 const DataForm = (props) => {
-  //% State hooks
-  const [username, setUsername] = useState("");
-  const [age, setAge] = useState("");
-  //% Handlers
-  const input1Handler = function (e) {
-    setUsername(e.target.value);
-  };
-  const input2Handler = function (e) {
-    setAge(e.target.value);
-  };
+  //% Refs
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  //% Handler (only need one for submit with refs instead of states)
   const submitHandler = function (e) {
+    //$ We place ref current values in the handler scope on purpose
+    //$ By now, the JSX has rendered, so the refs have values that aren't undefined
+    //$ undefined.current.value generates an error otherwise
+    let nameInputValue = nameInputRef.current.value;
+    let ageInputValue = ageInputRef.current.value;
+
     e.preventDefault();
     // If you get bad data, render the modal in App.js
-    if (username.trim().length === 0 || age <= 0) {
+    if (nameInputValue.trim().length === 0 || ageInputValue <= 0) {
       props.renderModal();
       return; // prevent sending data to list
     }
     // If data's good, render new data in EntryList.js
     // We pass data to App.js, unshift the array there, then send the data to EntryList.js
-    props.addToList(username, age);
-    // Clear the input fields while you're at it
-    setUsername("");
-    setAge("");
+    props.addToList(nameInputValue, ageInputValue);
+    //$ Clear the input fields
+    //$ At this point, the useRef is now equal to an object
+    nameInputRef.current.value = "";
+    ageInputRef.current.value = "";
   };
 
   return (
     <Card>
       <form className={styles.formContainer}>
         <label className={styles.label1}>Username</label>
-        <input type="text" value={username} onChange={input1Handler} />
+        <input type="text" ref={nameInputRef} />
         <label className={styles.label2}>Age (Years)</label>
-        <input type="number" step="1" value={age} onChange={input2Handler} />
+        <input type="number" step="1" ref={ageInputRef} />
         <button onClick={submitHandler} className={styles.btn} type="submit">
           Add User
         </button>
